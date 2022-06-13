e4ccc05bda4f9af2ddc6f4affa42aaceb98d3f36
IDAGIZA <ruthidagiza@gmail.com>
2022-06-13 22:18:45 +0300

    commit tree d8e8dd9b085c09a75098513de0aaa2d0d2325dc5

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const employee = {
+    name: "ruth",
+    streetAddress: "kahawa-sukari",
+  };
+
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  function deleteFromEmployeeByKey(employee, key) {
+    const copyEmployee = { ...employee };
+    delete copyEmployee[key];
+    return copyEmployee;
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
